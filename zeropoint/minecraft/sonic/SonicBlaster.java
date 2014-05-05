d7266554dd2590d7eaaa64162db46ab4d78300b2
Zero Point <zeropointmcdev@gmail.com>
2014-05-05 17:22:15 -0400

    Allowed changing of modes on the Sonic Blaster

--- zeropoint/minecraft/sonic/SonicBlaster.java
+++ zeropoint/minecraft/sonic/SonicBlaster.java
@@ -10,10 +10,13 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.Event.Result;
@@ -24,10 +27,12 @@ import zeropoint.minecraft.core.util.EnumBlockSide;
 import zeropoint.minecraft.core.util.Log;
 import zeropoint.minecraft.core.util.manip.EnchantHelper;
 import zeropoint.minecraft.core.util.manip.InventoryHelper;
+import zeropoint.minecraft.core.util.manip.ItemStackHelper;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 
 @SuppressWarnings("javadoc")
-public class SonicBlaster extends Item {
+public class SonicBlaster extends Item implements IRecipe {
 	private static final Logger LOG = Log.getLogger(GTSonic.name);
 	public SonicBlaster(int id) {
 		super(id);
@@ -36,16 +41,37 @@ public class SonicBlaster extends Item {
 		this.setUnlocalizedName("gtweaks.sonic.sonicblaster");
 		this.setTextureName("gtweaks:blaster");
 		MinecraftForge.EVENT_BUS.register(this);
+		GameRegistry.addRecipe(this);
 	}
 	@Override
 	public void addInformation(ItemStack is, EntityPlayer player, List l, boolean B) {
-		l.add(ChatMsg.CYAN + "Sneak and right click a block to use.");
-		l.add(ChatMsg.SILVER + "Instantly breaks blocks in a 3x3 around");
-		l.add(ChatMsg.SILVER + "the block you hit. Attempts to place drops");
-		l.add(ChatMsg.SILVER + "directly into your inventory.");
-		l.add("");
+		if (is.stackTagCompound == null) {
+			is.stackTagCompound = new NBTTagCompound("tag");
+		}
+		NBTTagCompound tag = is.stackTagCompound;
+		byte mode = tag.getByte("mode");
+		if (mode == 0) {
+			l.add(ChatMsg.SKY + "Precision mode");
+			if (B) {
+				l.add(ChatMsg.SILVER + "Instantly break the block you hit");
+			}
+			l.add(ChatMsg.GRAY + "Place in a crafting grid to change to area mode");
+		}
+		else if (mode == 1) {
+			l.add(ChatMsg.RED + "Area mode");
+			if (B) {
+				l.add(ChatMsg.SILVER + "Instantly breaks 3x3 around the block you hit");
+			}
+			l.add(ChatMsg.GRAY + "Place in a crafting grid to change to precision mode");
+		}
+		else {
+			tag.setByte("mode", (byte) 0);
+			this.addInformation(is, player, l, B);
+			return;
+		}
+		l.add(ChatMsg.LIME + "Attempts to place drops directly into your inventory.");
 		l.add(ChatMsg.MAROON + "Doesn't collect items from inventories!");
-		l.add(ChatMsg.RED + "Careful when using on chests!");
+		l.add(ChatMsg.MAROON + "Be careful when using on chests!");
 	}
 	@Override
 	public EnumAction getItemUseAction(ItemStack par1ItemStack) {
@@ -56,21 +82,39 @@ public class SonicBlaster extends Item {
 		return EnumRarity.rare;
 	}
 	@Override
+	public void onCreated(ItemStack stack, World world, EntityPlayer player) {
+		if (stack.stackTagCompound == null) {
+			stack.stackTagCompound = new NBTTagCompound("tag");
+		}
+		NBTTagCompound tag = stack.getTagCompound();
+		tag.setByte("mode", tag.getByte("mode"));
+	}
+	@Override
 	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int cX, int cY, int cZ, int blockSideId, float hitX, float hitY, float hitZ) {
-		return false;
+		return true;
 	}
 	@Override
 	public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int blockSideId, float hitX, float hitY, float hitZ) {
-		if ( !player.isSneaking()) {
-			return false;
-		}
 		if ( !world.isRemote) {
 			if ( !player.capabilities.isCreativeMode && !useFuel(player)) {
 				new ChatMsg(ChatMsg.RED + "Your sonic blaster is out of fuel!").send(player);
 				new ChatMsg(ChatMsg.GRAY + "You need " + GTSonic.blasterFuelCount + " of " + GTSonic.blasterFuelName + ChatMsg.GRAY + " to use your blaster").send(player);
 				return false;
 			}
-			sonicBlock(x, y, z, world, player, EnumBlockSide.getByInt(blockSideId));
+			ItemStack held = player.getCurrentEquippedItem();
+			if (held.stackTagCompound == null) {
+				held.stackTagCompound = new NBTTagCompound("tag");
+			}
+			if (held.stackTagCompound.getByte("mode") == 0) {
+				doDrops(world, x, y, z, player);
+			}
+			else if (held.stackTagCompound.getByte("mode") == 1) {
+				sonicBlock(x, y, z, world, player, EnumBlockSide.getByInt(blockSideId));
+			}
+			else {
+				new ChatMsg("*beep* " + ChatMsg.GRAY + ChatMsg.ITALIC + "[System rebooted]").send(player);
+				held.stackTagCompound.setByte("mode", (byte) 0);
+			}
 		}
 		player.inventory.onInventoryChanged();
 		player.inventoryContainer.detectAndSendChanges();
@@ -138,7 +182,6 @@ public class SonicBlaster extends Item {
 			LOG.warning("Null block hit‽");
 			return;
 		}
-		LOG.info("Handling block #" + id + " at (" + x + ", " + y + ", " + z + ")");
 		if (hit instanceof BlockDoor) {
 			if ((world.getBlockId(cX, cY - 1, cZ) == id) && ((world.getBlockMetadata(cX, cY, cZ) & 8) != 0)) {
 				// They hit the top half
@@ -146,10 +189,8 @@ public class SonicBlaster extends Item {
 				return;
 			}
 		}
-		LOG.info("Dropping block #" + id + " at (" + x + ", " + y + ", " + z + ")");
 		List<ItemStack> drops = hit.getBlockDropped(world, cX, cY, cZ, world.getBlockMetadata(cX, cY, cZ), EnchantHelper.getEnchantmentLevel(Enchantment.fortune.effectId, player.getCurrentEquippedItem()));
 		for (ItemStack drop : drops) {
-			LOG.info("Dropping " + drop.stackSize + " of " + drop.getDisplayName() + " (item #" + drop.itemID + ")");
 			// The old method of handling direct drop-to-inventory
 			// transitions didn't play well with Forestry's backpacks.
 			// Unfortunately, NEITHER method works right in creative.
@@ -165,6 +206,11 @@ public class SonicBlaster extends Item {
 		}
 		world.setBlockToAir(cX, cY, cZ);
 	}
+	@Override
+	public boolean hasEffect(ItemStack stack, int pass) {
+		ItemStack is = ItemStackHelper.fix(stack);
+		return is.stackTagCompound.getByte("mode") > 0;
+	}
 	@SuppressWarnings("static-method")
 	@ForgeSubscribe
 	public void onPickup(EntityItemPickupEvent evt) {
@@ -176,5 +222,54 @@ public class SonicBlaster extends Item {
 				evt.setResult(Result.DENY);
 			}
 		}
+	}
+	@Override
+	public boolean matches(InventoryCrafting grid, World world) {
+		boolean found = false;
+		for (int i = 0; i < grid.getSizeInventory(); ++i) {
+			ItemStack slot = grid.getStackInSlot(i);
+			if (slot != null) {
+				if (slot.itemID != this.itemID) {
+					return false;
+				}
+				if (found) {
+					return false;
+				}
+				found = true;
+			}
+		}
+		return found;
+	}
+	@Override
+	public ItemStack getCraftingResult(InventoryCrafting grid) {
+		if ( !this.matches(grid, null)) {
+			return null;
+		}
+		for (int i = 0; i < grid.getSizeInventory(); ++i) {
+			ItemStack slot = grid.getStackInSlot(i);
+			if (slot != null) {
+				if (slot.itemID == this.itemID) {
+					ItemStack out = ItemStackHelper.fix(slot);
+					NBTTagCompound tag = out.stackTagCompound;
+					byte mode = tag.getByte("mode");
+					if (mode == 0) {
+						tag.setByte("mode", (byte) 1);
+					}
+					else {
+						tag.setByte("mode", (byte) 0);
+					}
+					return out;
+				}
+			}
+		}
+		return null;
+	}
+	@Override
+	public int getRecipeSize() {
+		return 1;
+	}
+	@Override
+	public ItemStack getRecipeOutput() {
+		return null;
 	}
 }
